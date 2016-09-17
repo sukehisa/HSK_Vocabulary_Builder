95ec90c5bc320f9b1dc7b38240c55f7404453d85
yusuke <yusuke.kumur@gmail.com>
2016-09-17 10:54:59 +0900

    - Added Excel read functionality - Added smarter Selenium Wait functionality

--- hsk_scrape.py
+++ hsk_scrape.py
@@ -3,14 +3,25 @@
 import sys
 import time
 import lxml
-from lxml import etree
+import xlrd
 
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
 
-def hsk_vocab_scrape(url):
+
+def excel_read(xlsfilename, sheet_idx):
+	book = xlrd.open_workbook(xlsfilename)
+	sheet = book.sheet_by_index(sheet_idx)
+	elems = []
+	# skip the first row
+	for row in range(1, sheet.nrows):
+        # read A column
+		elems.append(sheet.cell(row, 0).value)
+	return elems
+
+def hsk_vocab_scrape(wd, url):
     #driver = webdriver.PhantomJS(service_log_path=None)
-    wd = webdriver.Firefox()
     wd.get(url)
     
     # Get a mouse
@@ -20,43 +31,40 @@ def hsk_vocab_scrape(url):
     mh = wd.find_element_by_xpath("/html/body[@class='nomarginpadding ']/div[@id='contentarea']/table/tbody/tr/td[@class='resultswrap']/table[@class='wordresults']/tbody/tr[@class='row']/td[@class='actions']/div[@class='nonprintable']/div/div[@class='c']/img")
     mouse.move_to_element(mh).perform
 
-    
-    mc = wd.find_element_by_xpath("/html/body[@class='nomarginpadding ']/div[@id='contentarea']/table/tbody/tr/td[@class='resultswrap']/table[@class='wordresults']/tbody/tr[@class='row']/td[@class='actions']/div[@class='nonprintable']/div/div[@class='e']/div[@class='b']/a[3]")
-    
+    # Click "Copy this entry in plain text"
+    #mc = wd.find_element_by_xpath("/html/body[@class='nomarginpadding ']/div[@id='contentarea']/table/tbody/tr/td[@class='resultswrap']/table[@class='wordresults']/tbody/tr[@class='row']/td[@class='actions']/div[@class='nonprintable']/div/div[@class='e']/div[@class='b']/a[3]")
+    mc = wd.find_element_by_xpath("/html/body[@class='nomarginpadding ']/div[@id='contentarea']/table/tbody/tr/td[@class='resultswrap']/table[@class='wordresults']/tbody/tr[@class='row']/td[@class='actions']/div[@class='nonprintable']/div/div[@class='e']/div[@class='b']/a[@title='Copy this entry in plain text']")                               
     mouse.click(mc).perform()
     
     # wait until it's loaded
-    time.sleep(5)    
-    
-    #print(wd.page_source)
-    html = wd.page_source.encode('utf-8')
-    
+    #time.sleep(5)
+    WebDriverWait(wd, 5).until(lambda d: d.find_element_by_xpath("/html/body[@class='nomarginpadding ']/div[@id='contentarea']/table/tbody/tr/td[@class='resultswrap']/table[@class='wordresults']/tbody/tr[2]/td/table").is_displayed())
 
-    #print(html)
-
-    #elem = driver.find_element_by_id("section_word_simple")
-    #element.click()
-    #driver.save_screenshot('click.png')
-
-    #mouse = webdriver.ActionChains(self.webdriver)
-    #element = wd.find_element_by_xpath("//a[@name=\"buy_button_link> \"]")
-    #mouse.move_to_element(element).perform()
-
+    #driver.save_screenshot('click.png')    
+    html = wd.page_source.encode('utf-8')    
+    # Now scrape the targeta
     soup = BeautifulSoup(html, "lxml")
-    #header = soup.find('//*[@id="contentarea"]/table/tbody/tr/td/table/tbody/tr[2]/td/')
-    target = soup.find("html").find("body").find("div", id='contentarea').find("td", class_="resultswrap").find("table", class_="wordresults").find("table", attrs={"cellpadding":"0", "cellspacing":"0"}).textarea.string
-    print(target)
-
-
-    
-
-    #htmlparser = etree.HTMLParser()
-    #tree = etree.parse(html, htmlparser)
-    #tree.xpath('//*[@id="contentarea"]/table/tbody/tr/td/table/tbody/tr[2]/td/')
-
-    
+    try:
+        target = soup.find("html").find("body").find("div", id='contentarea').find("td", class_="resultswrap").find("table", class_="wordresults").find("table", attrs={"cellpadding":"0", "cellspacing":"0"}).textarea.string
+    except:
+        target = "NOTFOUND"
+    return target
 
 if __name__ == '__main__':
-    url = 'http://www.mdbg.net/chindict/chindict.php?page=worddict&wdrst=0&wdqb=%E4%BB%B7%E9%92%B1'
-    #print(url)
-    hsk_vocab_scrape(url)
+    meaning_list = []
+    wd = webdriver.Firefox()
+    
+    urlbase = 'http://www.mdbg.net/chindict/chindict.php?page=worddict&wdrst=0&wdqb={voca}'   
+    vocas = excel_read("/Users/yusuke/Dropbox/Works/Python/HSK_Vocabulary_Builder/HSK_Vocabulary.xlsx", 0)
+
+    i = 0
+    for vocabulary in vocas:
+        result = hsk_vocab_scrape(wd, urlbase.format(voca=vocabulary))
+        meaning_list.append(result) 
+        i += 1
+        if i > 5:
+            break
+
+    out = open('result.txt', 'w')
+    for meaning in meaning_list:
+       out.write("%s\n" % (meaning))
